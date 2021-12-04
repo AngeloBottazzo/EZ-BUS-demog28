2f671c9acea30e3bf858445eff466a62135149f3
Angelo <angelo.bot@outlook.it>
2021-12-04 18:28:45 +0100

    base pagina web

---
+++ ez-bus-demog28-ui/home.js
@@ -0,0 +1,4 @@
+const home={template:`<div style="flex: 1; display: flex; flex-direction: column; justify-content: space-evenly">
+<router-link to="/acquista-biglietto" class="btn btn-primary btn-lg" tabindex="-1" role="button">Acquista biglietto</router-link >
+<router-link  to="/biglietti-acquistati" class="btn btn-primary btn-lg" tabindex="-1" role="button">Biglietti acquistati precedentemente</router-link >
+</div>`}
