8b69aabbfb4e60fcb7bd7f5ba4a0e705c4121aa8
Angelo <angelo.bot@outlook.it>
2021-12-15 10:13:18 +0100

    chiarimento su un errore

--- ez-bus-demog28-server/index.js
+++ ez-bus-demog28-server/index.js
@@ -402,7 +402,8 @@ app.get('/biglietti', (request, response) => {
 app.get('/viaggi-tra-stazioni', (request, response) => {
     let data_viaggio = moment(request.query.data_viaggio)
     if (!('stazione_partenza' in request.query) || !('stazione_arrivo' in request.query) || !data_viaggio.isValid()) {
-        response.sendStatus(400);
+        response.status(400);
+        response.send("Dati non completi")
         return;
     }
 
