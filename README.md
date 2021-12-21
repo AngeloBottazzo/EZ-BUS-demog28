b26078249f4974f9aa1b3aa706c9398f4c3e7e42
Elia <elia.gatti@studenti.unitn.it>
2021-12-21 11:15:13 +0100

    Aggiunta di 2 test case e modifiche sul file readme.md

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ Questo progetto è stato svolto dal gruppo G28, composto da:
 <li> <b> Elia Gatti </b> </li>
 <li> <b> Leonardo Lucini </b> </li>
 </ul>
-Per qualsiasi info sul progetto scrivere una mail <strong> angelo.bottazzo@studenti.unitn.it </strong>
+Per qualsiasi info sul progetto scrivere una mail a <strong> angelo.bottazzo@studenti.unitn.it </strong>
 
 ## Descrizione della repository
 L'idea della repository creata è quella di mostrare l'utilizzo della applicazione nell'azioni riferite alla gestione/acquisto di biglietti. Per arrivare a questo traguardo abbiamo costruito: 
@@ -22,6 +22,6 @@ Nella home abbiamo inserito solo 2 bottoni che servono per navigare tra le due a
 ### Acquista biglietto
 Nella schermata di acquisto biglietto l'utente deve compilare un form inserendo la tratta da selezionare e qualche dato personale per identificare chi utilizza il biglietto. 
 ### Pagina di acquisto
-Nella pagina di acquisto l'utente ha la possibilità di visualizzare il prezzo del biglietto, selezionare, e successivamente inserire i dati, il metodo di pagamento preferito.
+Nella pagina di acquisto l'utente ha la possibilità di visualizzare il prezzo del biglietto, accettare i termini e condizioni e infine pagare il biglietto.
 ### Biglietti acquistati
 Nella schermata è possibile vedere i biglietti acquistati dall'utente
--- ez-bus-demog28-test/index.js
+++ ez-bus-demog28-test/index.js
@@ -35,5 +35,36 @@ server.connettiDatabaseEPrendiApp().then((app)=>{
                 assert.end();
             })
     });
+    
+    test('TEST 3: inserimento incorretto (manca la stazione di arrivo) di un biglietto dentro la collezione di biglietti',function (assert) {
+        request(app)
+            .post('/biglietti')
+            .send({
+                "stazione_partenza": "61ab9eb31e607d0f2cce7c58",
+                "stazione_arrivo": "",
+                "viaggio": "61b3f64ece9723f367f3a842",
+                "data_viaggio": "2021-12-23",
+                "nome": "Gino",
+                "cognome": "Pastino"
+            })
+            .expect(400)
+            .end((err,res) => {
+                if (err) {
+                    console.log(new Error('An error occured with the biglietto adding API, err: ' + err))
+                }
+                assert.error(err,'No error');
+                assert.end();
+            })
+    });
 
+    test('TEST 4: reperimento dei biglietti precedentemente acquistati',function (assert) {
+        request(app)
+            .get('/biglietti')
+            .expect(200)
+            .end((err,res) => {
+                assert.true(res.body.length > 0, 'Nessun biglietto ricevuto')
+                assert.error(err,'No error');
+                assert.end();
+            })
+    });
 })
